fee67e00349add605e69d1a40446500e4300f0eb
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-05-28 04:44:59 +0900

    docker file create

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
+  preview: {
+    port: 3001,
+  },
   plugins: [react()],
-})
+});
